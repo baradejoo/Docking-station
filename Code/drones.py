6eac9ca08edb7386581f9b198c04d84bbb3f757d
baradejoo <kamil.baradziej@racing.agh.edu.pl>
2021-06-03 15:12:33 +0200

    classes improvements

--- Code/drones.py
+++ Code/drones.py
@@ -2,24 +2,37 @@ from typing import List, Tuple
 
 
 class Drone:
+    """Class containing drone parameters"""
+
     def __init__(self, id_, drone_param_: Tuple):
+        """
+        :param id: id of each drone
+        :param x_0: x coordinate of drone at the map
+        :param y_0: y coordinate of drone at the map
+        """
         self.id = id_
-        self.x0 = x0
-        self.y0 = y0
+        self.x_0 = drone_param_[0]
+        self.y_0 = drone_param_[1]
+
+    def __str__(self) -> str:
+        return f"Drone no. {self.id}, coordinates: [{self.x_0},{self.y_0}]".format(self=self)
 
 
 class Individual:
     """Class containing individual chromosome and rating"""
 
-    def __init__(self, station_coordinates_: Tuple, prob_: float = 0):
+    def __init__(self, id_: int, station_coordinates_: List, prob_: float = 0):
         """
-        :param chromosome: opisac to
+        :param id: id of each individual
+        :param chromosome: described one individual (station) as 2 coordinates: x and y
+        :param obj_fcn: cost function which it's minimise
+        :param prob_: probability of choosing this individual
         """
-
+        self.id = id_
         self.chromosome = station_coordinates_
         self.obj_fcn = 0
         self.prob = prob_
 
-    def __str__(self):
-        return str(self.ch_t) + ' ' + str(self.ch_p) + ' ' + str(self.prob) + ' ' + str(self.obj_fcn) + ' ' + \
-               str(len(self.ch_p))
+    def __str__(self) -> str:
+        return f"Individual no. {self.id}, chromosome: {self.chromosome}, probability: {self.prob}, " \
+               f"cost function: {self.obj_fcn}".format(self=self)
