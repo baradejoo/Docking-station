457971156fe62ebb3202e938c52e390161aacd72
baradejoo <kamil.baradziej@racing.agh.edu.pl>
2021-06-23 17:57:33 +0200

    draw drones on the map

--- Code/visualisation.py
+++ Code/visualisation.py
@@ -1,7 +1,11 @@
 from Code.genetic_algorithm import Individual, Drone
 
+import os
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 from typing import List
+from matplotlib.offsetbox import TextArea, DrawingArea, OffsetImage, AnnotationBbox
+from PIL import Image
 
 
 def print_pop(pop, msg=""):
@@ -13,6 +17,17 @@ def print_pop(pop, msg=""):
 
 
 def visualisation(stations: List[Individual], drones_list: List[Drone], stations_radius: int, graph_size):
+    filename = "../drone.png"
+    drone_img = Image.open(filename, 'r')
+    fixed_height = 50
+    height_percent = (fixed_height / float(drone_img.size[1]))
+    width_size = int((float(drone_img.size[0]) * float(height_percent)))
+    drone_img = drone_img.resize((width_size, fixed_height), Image.NEAREST)
+
+    text_img = Image.new('RGBA', (49, 50), (0, 0, 0, 0))
+    text_img.paste(drone_img, (0, 0), mask=drone_img)
+    drone_box = OffsetImage(text_img, zoom=1)
+
     stations_center_x = [i.chromosome[0] for i in stations]
     stations_center_y = [i.chromosome[1] for i in stations]
     drones_x = [d.x for d in drones_list]
@@ -26,7 +41,10 @@ def visualisation(stations: List[Individual], drones_list: List[Drone], stations
         circle = plt.Circle((stations_center_x[idx], stations_center_y[idx]), stations_radius, color='b', fill=False)
         ax.add_patch(circle)
 
+    for idx in range(0, len(drones_x)):
+        ab = AnnotationBbox(drone_box, (drones_x[idx], drones_y[idx]), frameon=False)
+        ax.add_artist(ab)
     ax.plot(stations_center_x, stations_center_y, 'b.', markersize=40)
-    ax.plot(drones_x, drones_y, 'kx', markersize=15)
+    # ax.plot(drones_x, drones_y, 'o')
     fig.set_size_inches(20, 20)
     fig.show()
